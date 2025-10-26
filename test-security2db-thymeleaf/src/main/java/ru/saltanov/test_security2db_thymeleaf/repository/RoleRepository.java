49bc50ade1291ac6341700ffa6a3152406243aa8
salat <101834810+sa1lut@users.noreply.github.com>
2025-10-27 02:10:48 +0500

    lr8

---
+++ test-security2db-thymeleaf/src/main/java/ru/saltanov/test_security2db_thymeleaf/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package ru.saltanov.test_security2db_thymeleaf.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.saltanov.test_security2db_thymeleaf.entity.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
