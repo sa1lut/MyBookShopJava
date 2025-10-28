37c09b74f144f794f661bf5631505757830e1942
salat <101834810+sa1lut@users.noreply.github.com>
2025-10-28 14:45:26 +0500

    Delete MySecondTestAppSpringBoot directory

--- MySecondTestAppSpringBoot/src/main/java/ru/saltanov/MySecondTestAppSpringBoot/service/ModifyRequestService.java
+++
@@ -1,9 +0,0 @@
-package ru.saltanov.MySecondTestAppSpringBoot.service;
-
-import org.springframework.stereotype.Service;
-import ru.saltanov.MySecondTestAppSpringBoot.model.Request;
-
-@Service
-public interface ModifyRequestService {
-    void modify(Request request);
-}
--- MySecondTestAppSpringBoot/src/main/java/ru/saltanov/MySecondTestAppSpringBoot/service/QuarterlyBonusService.java
+++
@@ -1,9 +0,0 @@
-package ru.saltanov.MySecondTestAppSpringBoot.service;
-
-import org.springframework.stereotype.Service;
-import ru.saltanov.MySecondTestAppSpringBoot.model.Positions;
-
-@Service
-public interface QuarterlyBonusService {
-    double calculate(Positions positions, double salary, double bonus, int workDays);
-}
